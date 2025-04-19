fe2ad09b6d46b48ffbcf0e240333d01e02f1d74a
Lucaledesma <lucaledesma55@gmail.com>
2025-04-19 11:06:54 -0300

    First Commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/********** NAVBAR MOBILE **********/
+let navBar = document.querySelector("#navbar");
+let abrirMenu = document.querySelector("#abrir-menu");
+let cerrarMenu = document.querySelector("#cerrar-menu");
+
+abrirMenu.addEventListener("click", () => {
+    navBar.classList.add("visible");
+});
+
+cerrarMenu.addEventListener("click", () => {
+    navBar.classList.remove("visible");
+});
